22c5c6c923d4431e867722f886f75cdc97837765
luhongjin <luhongjin@hztianque.com>
2022-07-11 14:37:38 +0800

    删除错误提交目录

--- backend/gridcloud-campusepidemic/gridcloud-campusepidemic-api/src/main/java/com/tianque/grid/ce2/infrastructure/server/dubbo/DemoQueryDubboApi.java
+++
@@ -1,7 +0,0 @@
-package com.tianque.grid.ce2.infrastructure.server.dubbo;
-
-import io.swagger.annotations.Api;
-
-@Api(tags = "基本信息dubbo接口")
-public class DemoQueryDubboApi {
-}
--- backend/gridcloud-campusepidemic/gridcloud-campusepidemic-repository/src/main/java/com/tianque/grid/ce2/infrastructure/aop/GisAopService.java
+++
@@ -1,8 +0,0 @@
-package com.tianque.grid.ce2.infrastructure.aop;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class GisAopService {
-
-}
